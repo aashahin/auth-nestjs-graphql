6c9c7a7ba147496ae9a760476c269320659771f0
Abdelrahman Shaheen <abshahin8@outlook.com>
2023-12-06 11:36:06 +0200

    The app is running!

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule} from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UserModule,
+    GraphQLModule.forRootAsync({
+      imports: [ConfigModule,AppModule],
+      driver: ApolloDriver,
+      useFactory: async ()=> {
+        return {
+          playground: true,
+          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+          sortSchema: true,
+        };
+      }
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
